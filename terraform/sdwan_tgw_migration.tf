3bc481f52269109a455bd62e3b44c43b9a45ca56
Nathapon Puangchucharoen <nathapua@nm72601276.metrosystems.co.th>
2026-05-05 14:04:45 +0700

    fix: restore tgwrt_sdwan resource declaration

--- terraform/sdwan_tgw_migration.tf
+++ terraform/sdwan_tgw_migration.tf
@@ -1,8 +1,11 @@
 #############################################
 # SD-WAN TGW Migration
 # SD-WAN VPC + TGW Connect + BGP moved to sdwan_vpc.tf
-# This file kept for static routes only
+# This file kept for SD-WAN RT + static routes
 #############################################
 
-# SD-WAN resources are now in sdwan_vpc.tf
-# This file is intentionally minimal
+# SD-WAN TGW Route Table
+resource "aws_ec2_transit_gateway_route_table" "tgwrt_sdwan" {
+  transit_gateway_id = aws_ec2_transit_gateway.lab_tgw.id
+  tags = { Name = "${var.project_name}-SDWAN-TGW-RT-01" }
+}
