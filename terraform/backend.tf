c1cfb9080281d93b8a07cc0d788235fcc39bd72e
Nathapon Puangchucharoen <nathapua@nm72601276.metrosystems.co.th>
2026-05-05 13:40:40 +0700

    fix: upgrade terraform to 1.9.0 and skip region validation for ap-southeast-7

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "network-firewall-lab-176501510816-tfstate"
-    key            = "lab/terraform.tfstate"
-    region         = "ap-southeast-7"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+    bucket                      = "network-firewall-lab-176501510816-tfstate"
+    key                         = "lab/terraform.tfstate"
+    region                      = "ap-southeast-7"
+    encrypt                     = true
+    dynamodb_table              = "terraform-state-lock"
+    skip_region_validation      = true
+    skip_credentials_validation = true
   }
 }
